02a94bb46fc429ff5fe0f9159584e0eb1b3db315
zhs007 <>
2020-11-03 18:59:34 +0800

    upd

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ USER root
 
 RUN wget -O /usr/local/share/fonts/SourceHanSans-Normal.ttc https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTC/SourceHanSans-Normal.ttc
 
-RUN pip install grpcio grpcio-tools plotly ipywidgets
+RUN pip install grpcio grpcio-tools plotly ipywidgets kaleido
--- home/cnfunds/libtrdb2.py
+++ home/cnfunds/libtrdb2.py
@@ -5,6 +5,8 @@ import tradingdb2_pb2_grpc
 import yaml
 from datetime import datetime
 import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
 def loadConfig(fn: str):
@@ -43,3 +45,27 @@ def showCNFund(cfg, code):
         cfg['servaddr'], cfg['token'], 'cnfunds', code, 0, 0)
     fig = px.line(dffund, x="date", y="close", title=code)
     fig.show()
+
+
+def showCNFunds(cfg, codes, rows: int, cols: int):
+    titles = []
+    i = 0
+    for row in range(0, rows):
+        for col in range(0, cols):
+            titles.append(codes[i])
+            i += 1
+
+    fig = make_subplots(
+        rows=rows, cols=cols,
+        subplot_titles=titles)
+
+    i = 0
+    for row in range(0, rows):
+        for col in range(0, cols):
+            dffund = getFundValues(
+                cfg['servaddr'], cfg['token'], 'cnfunds', codes[i], 0, 0)
+            fig.add_trace(go.Scatter(
+                x=dffund['date'], y=dffund['close']), row=row+1, col=col+1)
+            i += 1
+
+    fig.show(renderer="png")
