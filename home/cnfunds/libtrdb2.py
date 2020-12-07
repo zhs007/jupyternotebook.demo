ee13be324eb285213957a99df391a45d758f2793
zhs007 <sssxueren@gmail.com>
2020-12-07 21:42:03 +0800

    upd

--- home/cnfunds/libtrdb2.py
+++ home/cnfunds/libtrdb2.py
@@ -621,3 +621,30 @@ def buildPNLReport(lstpnl: list) -> pd.DataFrame:
         fv0['maxPerDownYear'].append(v['pnl'].maxPerDownYear)
 
     return pd.DataFrame(fv0)
+
+
+def genSimTradingParams(cfg, lstParams: list, ignoreCache: bool = False):
+    for i in range(len(lstParams)):
+        yield tradingdb2_pb2.RequestSimTrading(
+            basicRequest=trading2_pb2.BasicRequestData(
+                token=cfg['token'],
+            ),
+            params=lstParams[i],
+            ignoreCache=ignoreCache,
+            index=i,
+        )
+
+
+def simTradings(cfg, lstParams: list, ignoreCache: bool = False) -> list:
+    channel = grpc.insecure_channel(cfg['servaddr'])
+    stub = tradingdb2_pb2_grpc.TradingDB2Stub(channel)
+
+    lstRes = []
+    responses = stub.simTrading2(
+        genSimTradingParams(cfg, lstParams, ignoreCache))
+    for response in responses:
+        if len(response.pnl) > 0:
+            pnl = response.pnl[0]
+            lstRes.append({'title': '', 'pnl': pnl.total})
+
+    return lstRes
