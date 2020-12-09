671b51b9fc8ebfc18cf4eb7e055843e10e7913c8
zhs007 <sssxueren@gmail.com>
2020-12-09 14:06:14 +0800

    upd

--- home/cnfunds/libtrdb2.py
+++ home/cnfunds/libtrdb2.py
@@ -648,3 +648,60 @@ def simTradings(cfg, lstParams: list, ignoreCache: bool = False) -> list:
             lstRes.append({'title': pnl.title, 'pnl': pnl.total})
 
     return lstRes
+
+
+def calcPNLWinRateInYear(pnl: trading2_pb2.PNLAssetData, year: int) -> dict:
+    sellnums = 0
+    winnums = 0
+    buynums = 0
+
+    for v in pnl.lstCtrl:
+        dt = datetime.fromtimestamp(v.ts)
+        if dt.year == year:
+            if v.type == trading2_pb2.CtrlType.CTRL_SELL:
+                sellnums = sellnums + 1
+                if v.sellPrice > v.averageHoldingPrice:
+                    winnums = winnums + 1
+            if v.type == trading2_pb2.CtrlType.CTRL_BUY:
+                buynums = buynums + 1
+
+    ret = {'sellnums': sellnums, 'winnums': winnums,
+           'winrate': 0, 'buynums': buynums}
+    if sellnums > 0:
+        ret['winrate'] = winnums * 1.0 / sellnums
+
+    return ret
+
+
+def buildPNLWinRateReportYear(lstpnl: list) -> pd.DataFrame:
+    minyear = 0
+    maxyear = 0
+
+    for v in lstpnl:
+        sdt = datetime.fromtimestamp(v['pnl'].values[0].ts)
+        edt = datetime.fromtimestamp(
+            v['pnl'].values[len(v['pnl'].values) - 1].ts)
+
+        if minyear == 0 or sdt.year < minyear:
+            minyear = sdt.year
+
+        if maxyear == 0 or edt.year > maxyear:
+            maxyear = edt.year
+
+    fv0 = {
+        'title': [],
+    }
+
+    for y in range(minyear, maxyear + 1):
+        fv0['{}'.format(y)] = []
+
+        for v in lstpnl:
+            if y == minyear:
+                fv0['title'].append(v['title'])
+
+            ret = calcPNLWinRateInYear(v['pnl'], y)
+            fv0['{}'.format(y)].append(ret['winrate'])
+
+    # print(fv0)
+
+    return pd.DataFrame(fv0)
