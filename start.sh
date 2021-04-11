cae3059a4a3478f6b64868e4a9c20c93a1cb99e2
zhs007 <sssxueren@gmail.com>
2021-04-11 12:25:10 +0000

    upd

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 docker stop jupyternotebook
 docker rm jupyternotebook
 docker run -d \
+    -e CHOWN_HOME=yes -e CHOWN_HOME_OPTS='-R' \
     -v $PWD/home:/home/jovyan \
     --name jupyternotebook \
     -p 8888:8888 \
